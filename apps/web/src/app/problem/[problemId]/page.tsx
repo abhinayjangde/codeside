0e99f638fcb5edeb83f985b66cdc70a6cb1aac6b
Abhinay Jangde <abhinayjangde@gmail.com>
2024-12-11 21:34:06 +0530

    tabs are build and codeEditor components are restructures

--- apps/web/src/app/problem/[problemId]/page.tsx
+++ apps/web/src/app/problem/[problemId]/page.tsx
@@ -1,5 +1,5 @@
 
-import ProblemDescription from '@/components/ProblemDescription'
+
 import { getProblem } from '@/db/problem'
 import {
     ResizableHandle,
@@ -7,19 +7,11 @@ import {
     ResizablePanelGroup,
 } from "@/components/ui/resizable"
 
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
-import Link from "next/link"
-import { GoCodeSquare } from "react-icons/go";
-import { MdDescription } from "react-icons/md";
-import { IoTimeOutline } from "react-icons/io5";
-import { AiOutlineExperiment } from "react-icons/ai";
+
+
 import { Metadata } from "next";
 import CodeEditor from '@/components/CodeEditor'
+import ProblemDetails from '@/components/ProblemDetails'
 
 export const generateMetadata = async ({ params }: { params: { problemId: string } }): Promise<Metadata> => {
     const problem = await getProblem(params.problemId);
@@ -37,56 +29,18 @@ const Problem = async ({ params }: { params: { problemId: string } }) => {
     }
 
     return (
-        <>
-            <ResizablePanelGroup
-                direction="horizontal"
-                className="max-w-screen rounded-lg border min-w-screen"
-            >
-                <ResizablePanel defaultSize={50}>
-                    <div className="border border-gray-500 rounded-md min-h-screen dark:bg-dark">
-                        <div className="flex flex-row gap-2 px-2 py-2 border-b border-gray-300 dark:bg-slate">
-                            <div className="flex gap-1 font-semibold justify-center items-center">
-                                <MdDescription className="text-blue-600 text-xl" />
-                                <Link href={"/"}>Description |</Link>
-                            </div>
-                            <div className="flex gap-1 font-semibold justify-center items-center">
-                                <AiOutlineExperiment className="text-blue-600 text-xl" />
-                                <Link href={"/"}>Solutions |</Link>
-                            </div>
-                            <div className="flex gap-1 font-semibold justify-center items-center">
-                                <IoTimeOutline className="text-blue-600 text-xl" />
-                                <Link href={"/"}>Submissions</Link>
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-2 px-2 py-2 text-sm">
-                            <ProblemDescription description={problem.description} />
-                        </div>
-                    </div>
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel defaultSize={50}>
-                    <ResizablePanelGroup direction="vertical">
-                        <ResizablePanel defaultSize={75}>
-                            <div className="h-full border border-gray-500 rounded-md dark:bg-dark">
-                                <CodeEditor problem={problem} />
-                            </div>
-                        </ResizablePanel>
-                        <ResizableHandle />
-                        <ResizablePanel defaultSize={25}>
-                            <div className="h-full border border-gray-500 rounded-md dark:bg-dark">
-                                <div className="flex flex-row items-center gap-2 py-2 px-2 border-b border-gray-300 dark:bg-slate">
-                                    <GoCodeSquare className="text-green-400 text-xl" />
-                                    <h6 className="font-semibold">Testcases</h6>
-                                </div>
-                            </div>
-                        </ResizablePanel>
-                    </ResizablePanelGroup>
-                </ResizablePanel>
-            </ResizablePanelGroup>
+        <ResizablePanelGroup
+            direction="horizontal"
+            className="max-w-screen rounded-lg border min-w-screen"
+        >
+            <ProblemDetails description={problem.description} />
 
+            <ResizableHandle />
 
+            <CodeEditor problem={problem} />
 
-        </>
+        </ResizablePanelGroup>
+
     )
 }
 
---
+++ apps/web/src/components/ProblemDetails.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { MdDescription } from "react-icons/md";
+import { IoTimeOutline } from "react-icons/io5";
+import { AiOutlineExperiment } from "react-icons/ai";
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import ProblemDescription from '@/components/ProblemDescription'
+import { useState } from "react";
+
+const ProblemDetails = ({ description }: { description: string }) => {
+    const [activeTab, setActiveTab] = useState('description');
+    const renderContent = () => {
+        switch (activeTab) {
+            case 'description':
+                return <ProblemDescription description={description} />;
+            case 'solutions':
+                return <div>Solutions Component</div>;
+            case 'submissions':
+                return <div>Submissions Component</div>;
+            default:
+                return null;
+        }
+    };
+    return (
+        <ResizablePanel defaultSize={50}>
+            <div className="border border-gray-500 rounded-md min-h-screen dark:bg-dark">
+                <div className="flex flex-row gap-2 px-2 py-[0.39rem] border-b border-gray-300 dark:bg-slate">
+                    <div
+                        className={`flex gap-1 font-semibold justify-center items-center cursor-pointer ${activeTab === 'description' ? 'text-blue-600' : ''}`}
+                        onClick={() => setActiveTab('description')}
+                    >
+                        <MdDescription className="" />
+                        <span>Description |</span>
+                    </div>
+                    <div
+                        className={`flex gap-1 font-semibold justify-center items-center cursor-pointer ${activeTab === 'solutions' ? 'text-blue-600' : ''}`}
+                        onClick={() => setActiveTab('solutions')}
+                    >
+                        <AiOutlineExperiment className="" />
+                        <span>Solutions |</span>
+                    </div>
+                    <div
+                        className={`flex gap-1 font-semibold justify-center items-center cursor-pointer ${activeTab === 'submissions' ? 'text-blue-600' : ''}`}
+                        onClick={() => setActiveTab('submissions')}
+                    >
+                        <IoTimeOutline className="" />
+                        <span>Submissions |</span>
+                    </div>
+                </div>
+                <div className="p-4">
+                    {renderContent()}
+                </div>
+            </div>
+        </ResizablePanel>
+    )
+}
+
+export default ProblemDetails
