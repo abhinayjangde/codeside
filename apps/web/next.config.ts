38e7d1023be6221da450580b0aa7c708e8c63c88
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2025-02-27 06:51:26 +0000

    trying to setup github codespace

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental:{
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000', // localhost
+        'fictional-spoon-q6766wjpj563xr96-3000.app.github.dev' // codespace
+      ],
+    },
+  },
   images: {
     remotePatterns: [
       {
