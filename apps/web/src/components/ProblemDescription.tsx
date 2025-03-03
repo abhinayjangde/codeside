666800bf35a8b46984766d33727be92dfebf73e6
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2025-03-03 22:28:22 +0530

    apply styles in problem description section (#25)
    done

--- apps/web/src/components/ProblemDescription.tsx
+++ apps/web/src/components/ProblemDescription.tsx
@@ -1,13 +1,57 @@
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import matter from "gray-matter";
+import { remark } from "remark";
+import remarkRehype from "remark-rehype";
+import rehypeHighlight from "rehype-highlight";
+import rehypeStringify from "rehype-stringify";
+import { useEffect, useState } from "react";
 
 const ProblemDescription = ({ description }: { description: string }) => {
-  
+  const [content, setContent] = useState<string>("");
+
+  useEffect(() => {
+    const processContent = async () => {
+      // Parse front matter
+      const { data, content } = matter(description);
+      console.log(content)
+
+      // Convert Markdown to HTML with syntax highlighting
+      const processedContent = await remark()
+        .use(remarkRehype) // Convert Markdown -> HTML AST
+        .use(rehypeHighlight) // Apply syntax highlighting
+        .use(rehypeStringify) // Convert AST -> HTML string
+        .process(content);
+
+      setContent(processedContent.toString());
+    };
+
+    processContent();
+  }, [description]);
+
   return (
     <div>
-      <Markdown remarkPlugins={[remarkGfm]}>{description}</Markdown>
+       <style>
+        {`
+          h2{
+            font-size: 25px;
+          }
+          p{
+            margin: 5px 0px;
+          }
+          pre {
+            background: #282c34;
+            color: white;
+            padding: 10px;
+            border-radius: 5px;
+            overflow-x: auto;
+          }
+          code {
+            font-family: monospace;
+          }
+        `}
+      </style>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
-  )
-}
+  );
+};
 
-export default ProblemDescription
+export default ProblemDescription;
